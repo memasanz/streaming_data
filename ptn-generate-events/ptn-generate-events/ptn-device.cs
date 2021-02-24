356d173042c041d39abafb8b4f926343a3d8c0e3
Megan Masanz <memasanz@microsoft.com>
2021-02-23 18:59:12 -0600

    updated

---
+++ ptn-generate-events/ptn-generate-events/ptn-device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ptn_generate_events
+{
+    public class PTNDevice
+    {
+        public int DeviceId { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Value { get; set; }
+        public int Value_Quality { get; set; }
+
+        public string Summary;
+    }
+}
